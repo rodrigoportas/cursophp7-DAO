0337f8ae0ec09f5404ac1eca31ab4172ada1cb85
rodrigoportas <hoready_magrao@hotmail.com>
2018-06-25 03:27:57 -0300

    criams a classe usuario

--- index.php
+++ index.php
@@ -8,11 +8,25 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);*/
 
-$root = new Usuario();
 
-$root->loadById(3);
+//carrega um usuario
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
 
-echo $root;
+//carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
+
+//carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("ke");
+//echo json_encode($search);
+
+
+//carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("keke","87654323467");
+echo $usuario;
 
 ?>
